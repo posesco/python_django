1a64d5b41ed246e9c562b4bf3520779644c424f2
Jesus David Posada Escobar <posesco@gmail.com>
2021-07-06 21:31:26 -0500

    add password generator

---
+++ basicoPython/13.generadorPassword.py
@@ -0,0 +1,24 @@
+import random
+
+def generarPass():
+    mayus = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'V', 'X', 'Y', 'Z']
+    minus = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'v', 'x', 'y', 'z']
+    simbols = ['＃','＄', '％', '＆', '＊', '？', '＠', '～', '(', ')']
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    caracteres = mayus + minus + simbols 
+    password = []
+
+    for i in range(20):
+        caracter_random = random.choice(caracteres)
+        password.append(caracter_random)
+
+    password = ''.join(password)
+
+    return password
+
+def run():
+    password = generarPass()
+    print(f'Tu password es: {password}')
+    
+if __name__ == '__main__':
+    run()
