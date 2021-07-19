325bbb12aa9e31c2c46e0dd31ed2ab529d5bae66
Jesus David Posada Escobar <posesco@gmail.com>
2021-07-18 22:59:37 -0500

    update aproximaciones

--- pensamiento_computacional/5.aproximacion.py
+++ pensamiento_computacional/5.aproximacion.py
@@ -1,9 +1,10 @@
 objetivo = int(input('Escoge un entero: ')) 
-epsilon = 0.01
+epsilon = 0.001
 paso = epsilon**2
 respuesta = 0.0
 
 while abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:
+    print(abs(respuesta**2 - objetivo), respuesta)
     respuesta += paso
 
 if abs(respuesta**2 - objetivo) >= epsilon:
