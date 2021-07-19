7245a53dfb43181030a2f65fe8c1a83335dd70d8
Jesus David Posada Escobar <posesco@gmail.com>
2021-07-18 22:51:43 -0500

    aproximacion en pensamiento computacional

---
+++ pensamiento_computacional/5.aproximacion.py
@@ -0,0 +1,13 @@
+objetivo = int(input('Escoge un entero: ')) 
+epsilon = 0.01
+paso = epsilon**2
+respuesta = 0.0
+
+while abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:
+    respuesta += paso
+
+if abs(respuesta**2 - objetivo) >= epsilon:
+    print(f'No se encontro la raiz cuadrada de {objetivo}')
+else:
+    print(f'La raiz cuadrada de {objetivo} es igual a {respuesta}')
+
