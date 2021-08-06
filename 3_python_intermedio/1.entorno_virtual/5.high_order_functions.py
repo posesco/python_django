4a776634684f225854f020792d8ac77fc485d717
Jesus David Posada Escobar <jesus.posada@wiedii.co>
2021-08-05 22:52:45 -0500

    5.high_order_functions

--- 3_python_intermedio/1.entorno_virtual/5.high_order_functions.py
+++ 3_python_intermedio/1.entorno_virtual/5.high_order_functions.py
@@ -1,1 +1,8 @@
- 
+from functools import reduce
+my_list = [i for i in range(1, 11)]
+odd = list(filter(lambda x: x%2 !=0, my_list))
+squares = list(map(lambda x: x**2, my_list))
+all_multiple = reduce(lambda x, y: x*y, my_list)
+print(f'Numeros Primos: {odd}')
+print(f'Numeros Cuadrados: {squares}')
+print(f'Numeros Multiplicados: {all_multiple}')
