f3458fc9103b445deb9663249e689d7a21fca458
Jesus David Posada Escobar <jesus.posada@wiedii.co>
2021-08-05 22:09:51 -0500

    dict_comprenhensions

---
+++ 3_python_intermedio/1.entorno_virtual/3.dict_comprenhensions.py
@@ -0,0 +1,10 @@
+def run():
+    # numbers = {}
+    # for i in range(1, 101):
+    #     if i % 3 != 0:
+    #         numbers[i]= i**3
+    numbers = {i: i**3 for i in range(1, 101) if i % 3 != 0}
+    print(numbers)
+
+if __name__ == '__main__':
+    run()
