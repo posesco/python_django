37ac9e25327bab2740d8217ec98a4692a9c5625d
Jesus David Posada Escobar <jesus.posada@wiedii.co>
2021-08-05 22:18:29 -0500

    dict_comprenhensions

--- 3_python_intermedio/1.entorno_virtual/3.dict_comprenhensions.py
+++ 3_python_intermedio/1.entorno_virtual/3.dict_comprenhensions.py
@@ -3,7 +3,8 @@ def run():
     # for i in range(1, 101):
     #     if i % 3 != 0:
     #         numbers[i]= i**3
-    numbers = {i: i**3 for i in range(1, 101) if i % 3 != 0}
+    # numbers = {i: i**3 for i in range(1, 101) if i % 3 != 0}
+    numbers = {i: round(i**0.5,2) for i in range(1, 101)}
     print(numbers)
 
 if __name__ == '__main__':
